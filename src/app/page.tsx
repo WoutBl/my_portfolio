0f89bf952b81d67dc4af09d0d8429dc95cdd1656
Wout Blomme <wout.blomme@student.howest.be>
2023-08-04 13:17:46 +0200

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client'
 import Navbar from "../components/Navbar"
 
+
 export default function Home() {
+
+  
+
   return (
     <main  >
         <Navbar/>
-        <div className="flex items-center justify-center h-screen ">
+        <section className="flex items-center justify-center h-screen ">
           <div className="flex flex-col items-center text-text-primary-400">
             <div className="font-regular text-6xl mb-8">
               I'm Wout Blomme
@@ -14,8 +18,8 @@ export default function Home() {
               A Full Stack Web Developer
             </div>
           </div>
-        </div>
-        <div className="bg-primary-500 h-screen mx-20 rounded-3xl flex items-center">
+        </section>
+        <section className="bg-primary-500 h-screen mx-20 rounded-t-3xl rounded-b-none flex items-center">
           <div className="w-1/2">
 
           </div>
@@ -27,7 +31,13 @@ export default function Home() {
               Hi, I am from Maldegem, Belgium. I am a Full Stack Web Developer. I am currently studying at Howest University of Applied Sciences in Kortrijk. I am in my second year of the Bachelor MCT (Multimedia and Creative Technologies). I am a very motivated and ambitious person who is always eager to learn new things and always wants to achieve his goals. 
             </p>
           </div>
-        </div>
+        </section>
+        <section  className="bg-primary-500 h-screen mx-20  rounded-b-none flex items-center">
+          <div className="mb-10 font-bold text-4xl">
+            Skills
+          </div>
+        </section>
+
     
       
     </main>
