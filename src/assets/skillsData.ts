f981a624303cff782f62d65505675b03b93c6d13
BlommeWout <wout.blomme@student.howest.be>
2024-05-03 15:55:00 +0200

    added new skills

--- src/assets/skillsData.ts
+++ src/assets/skillsData.ts
@@ -31,6 +31,9 @@ import {
     SiTypescript,
     SiVite,
     SiXamarin,
+    SiVuedotjs,
+    SiEmberdotjs,
+    SiNestjs 
   } from 'react-icons/si'
   import { ISkill } from '../interfaces/ISkill'
   
@@ -44,39 +47,32 @@ import {
     { icon: SiTypescript, title: 'TypeScript', color: '#3178C6' },
     { icon: SiTailwindcss, title: 'Tailwind CSS', color: '#38B2AC' },
     { icon: SiSass, title: 'Sass', color: '#CC6699' },
-
     { icon: SiPython, title: 'Python', color: '#3776AB' },
     { icon: SiJson, title: 'JSON', color: '#000' },
     { icon: SiCsharp, title: 'C#', color: '#239120' },
     { icon: SiNextdotjs, title: 'Next.js', color: '#000000' },
     { icon: SiNodedotjs, title: 'Node.js', color: '#339933' },
+    { icon: SiNestjs, title: 'Nest.js', color: '#ea284d' },
     { icon: SiDotnet, title: '.NET', color: '#512BD4' },
     { icon: SiReact, title: 'React Native', color: '#61DAFB' },
     { icon: SiSocketdotio, title: 'Socket.io', color: '#010101' },
     { icon: SiPostman, title: 'Postman', color: '#FF6C37' },
     { icon: SiNetlify, title: 'Netlify', color: '#00C7B7' },
     { icon: SiVite, title: 'Vite', color: '#646CFF' },
+    { icon: SiVuedotjs, title: 'Vue', color: '#42b883' },
+    { icon: SiEmberdotjs, title: 'Ember', color: '#cf2d20' },
     { icon: SiReact, title: 'React', color: '#61DAFB' },
     { icon: SiChartdotjs, title: 'Chart.js', color: '#FF6384' },
-
-
-    { icon: SiMqtt, title: 'MQTT', color: '#660066' },
     { icon: SiMysql, title: 'MySQL', color: '#4479A1' },
     { icon: SiDocker, title: 'Docker', color: '#2496ED' },
     { icon: SiMongodb, title: 'MongoDB', color: '#47A248' },
     { icon: SiFirebase, title: 'Firebase', color: '#FFCA28' },
-    { icon: SiFirebase, title: 'Dapr', color: '#0D2192' },
     { icon: SiMicrosoftazure, title: 'Azure', color: '#0089D6' },
-
     { icon: SiGithub, title: 'GitHub', color: '#181717' },
     { icon: SiGit, title: 'Git', color: '#F05032' },
     { icon: SiGitkraken, title: 'GitKraken', color: '#179287' },
     { icon: SiArduino, title: 'Arduino', color: '#00979D' },
     { icon: SiRaspberrypi, title: 'Raspberry Pi', color: '#C51A4A' },
     { icon: SiNpm, title: 'NPM', color: '#CB3837' },
-    
-    
-   
-    
   ]
   
